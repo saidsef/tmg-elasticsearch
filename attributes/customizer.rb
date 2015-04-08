e12ce981db77f0a71444f5763053d19ff0f320bb
Said Sef <sefs@Saids-MacBook-Pro-2.local>
2015-04-08 12:40:13 +0100

    deb and rpm version bumps to 1.4.4

--- attributes/customizer.rb
+++ attributes/customizer.rb
@@ -1,3 +1,9 @@
 # === VERSION
 #
 default.elasticsearch[:version]       = "1.4.4"
+
+default.elasticsearch[:deb_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.deb"
+default.elasticsearch[:deb_sha] = "63471c1e9d5f6c5cf2c4d5fe20a9c27c86720810"
+
+default.elasticsearch[:rpm_url] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.noarch.rpm"
+default.elasticsearch[:rpm_sha] = "ec8b41c54a6d897479645b2507476e0824bc71db"
