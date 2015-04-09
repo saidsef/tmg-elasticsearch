dc5324bdb793f490d113a2307ec730cd5e29bd05
Said Sef <said.sef@telegraph.co.uk>
2015-04-09 10:14:31 +0100

    update conf files to v1.4.4

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,9 +19,17 @@ rewind :template => "elasticsearch.yml" do
 end
 
 rewind :template => "/etc/init.d/elasticsearch" do
-	source "elasticsearch.init.erb"
-	cookbook_name "tmg-elasticsearch"
-	owner 'root' and mode 0755
+  source "elasticsearch.init.erb"
+  cookbook_name "tmg-elasticsearch"
+  owner 'root' and mode 0755
+end
+
+rewind :template => "elasticsearch-env.sh" do
+  path   "#{node.elasticsearch[:path][:conf]}/elasticsearch-env.sh"
+  source "elasticsearch-env.sh.erb"
+  owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
+
+  notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
 end
 
 include_recipe 'elasticsearch::ebs'
