e3f121007328e71c008b9921b033f2ac510b6148
Said Sef <said.sef@telegraph.co.uk>
2015-04-09 14:43:56 +0100

    initial commit

--- attributes/customizer.rb
+++ attributes/customizer.rb
@@ -12,6 +12,8 @@ override['elasticsearch']['deb_sha'] = "63471c1e9d5f6c5cf2c4d5fe20a9c27c86720810
 override['elasticsearch']['rpm_url'] = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.noarch.rpm"
 override['elasticsearch']['rpm_sha'] = "ec8b41c54a6d897479645b2507476e0824bc71db"
 
+override['elasticsearch']['thread_stack_size'] = "512k"
+
 # === PLUGIN
 
 override['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.4.1'
