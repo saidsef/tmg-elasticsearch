fd552c97ad844384849b8b9470504af421867a75
Said Sef <said.sef@telegraph.co.uk>
2015-09-09 18:58:04 +0100

    WS-2960 package updates

--- Berksfile
+++ Berksfile
@@ -3,6 +3,4 @@ source "https://supermarket.chef.io"
 metadata
 
 cookbook 'apt', '~> 2.7
-cookbook 'logrotate'
-cookbook 'yum'
-cookbook 'java'
+
--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,4 @@ maintainer_email 'said.sef@telegrapg.co.uk'
 license          'All rights reserved'
 description      'TMG Installs/Configures tmg-elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          'elasticsearch'
+version          '0.2.0'
