95729b25e006505603704db24c11ad46401384c0
Said Sef <said.sef@telegraph.co.uk>
2015-09-09 21:48:42 +0100

    WS-2960 dependency update

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'All rights reserved'
 description      'TMG Installs/Configures tmg-elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
-depends          'elasticsearch', '~> 0.3.8'
+depends          'elasticsearch', '~ 0.3.8'
