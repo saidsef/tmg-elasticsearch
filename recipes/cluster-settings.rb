c073e88ca8515c02752be9d37d83608d94bb3775
Said Sef <said.sef@telegraph.co.uk>
2015-09-11 19:28:51 +0100

    WS-2068 wait for node to start

--- recipes/cluster-settings.rb
+++ recipes/cluster-settings.rb
@@ -1,7 +1,27 @@
 
+wait_between_attempts = 30
+curl_validation_command = "-s --head -XGET localhost:9200/_cluster/health | grep 200"
+max_attempts = 30
+execute "Waiting for ElasticSearch Node to Start -- please wait..." do
+	command <<-EOH
+		sleep 15
+		ATTEMP=0
+	  	while ! #{curl_validation_command} ; do
+	  		ATTEMPTS=$(expr ${ATTEMPTS} + 1)
+	  		echo "Waiting for ElasticSearch Node to Start -- please wait..."
+	  		sleep #{wait_between_attempts}
+
+	        if [ ${ATTEMPTS} -gt #{max_attempts} ] ; then
+	          echo "Max attempts reached... exiting with failure status."
+	          exit 100
+	        fi
+	  	done
+	EOH
+end
+
 execute "Update Bulk Queue Size" do
 	command <<-EOH
-	  sleep 4
+	  sleep 3
 	  curl -XPUT localhost:9200/_cluster/settings -d '
 		{
 		    "persistent" : {
