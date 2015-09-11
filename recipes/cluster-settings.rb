656292f0d8f2e718b5679314f147a4296acbee49
Said Sef <said.sef@telegraph.co.uk>
2015-09-11 14:35:45 +0100

    WS-2068 snapshot check bucket

--- recipes/cluster-settings.rb
+++ recipes/cluster-settings.rb
@@ -1,26 +1,21 @@
 
 execute "Update Bulk Queue Size" do
 	command <<-EOH
-	  sleep 15
+	  sleep 4
 	  curl -XPUT localhost:9200/_cluster/settings -d '
 		{
 		    "persistent" : {
-		        "threadpool.bulk.queue_size" : "500"
-		    },
-		    "transient" : {
-		        "indices.store.throttle.type" : "none"
-		    },
-		    "persistent" : {
+		        "threadpool.bulk.queue_size" : "500",
 		        "indices.store.throttle.max_bytes_per_sec" : "300mb"
 		    },
 		    "transient" : {
+		        "indices.store.throttle.type" : "none",
 		        "cluster.routing.allocation.disk.watermark.low" : "97%",
 		        "cluster.routing.allocation.disk.watermark.high" : "99%",
 		        "cluster.info.update.interval" : "1m",
 		        "cluster.routing.allocation.disk.include_relocations": true,
 		        "cluster.routing.allocation.cluster_concurrent_rebalance": 3
-		    },
-
+		    }
 		}'
 	EOH
 end
--- recipes/snapshots-infra.rb
+++ recipes/snapshots-infra.rb
@@ -3,7 +3,7 @@
 execute "Creating Infra Snapshot Entry" do
 	command <<-EHO
 		sleep 2
-		curl -XPOST http://localhost:9200/_snapshot/s3_infra_snapshot -d '
+		if (curl -s -XPUT http://localhost:9200/_snapshot/s3_infra_snapshot -d '
 		{
 		  "type": "s3",
 		  "settings": {
@@ -12,7 +12,20 @@ execute "Creating Infra Snapshot Entry" do
 		    "max_snapshot_bytes_per_sec": "500mb",
 		    "region": "eu-west-1"
 		  }
-		}'
+		}' | grep 'is not accessible on master node') then
+			echo "I CAN NOT CREATE SNAPSHOT - Fix bucket permissions"
+		else
+			curl -s -XPUT http://localhost:9200/_snapshot/s3_infra_snapshot -d '
+			{
+			  "type": "s3",
+			  "settings": {
+			    "bucket": "telegraph-elasticsearch-snapshots",
+			    "base_path" : "infra",
+			    "max_snapshot_bytes_per_sec": "500mb",
+			    "region": "eu-west-1"
+			  }
+			}'
+		fi
 	EHO
 end
 
